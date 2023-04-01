391b7386bab5f5a2364b6d6878a786e2d6722e67
Belén Albeza <belen@hey.com>
2023-04-01 14:32:07 +0200

    configure cypress with vite

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,20 @@
+import path from "path";
 import { defineConfig } from "cypress";
+import vitePreprocessor from "cypress-vite";
+
+const __dirname = path.resolve();
 
 export default defineConfig({
   video: false,
   chromeWebSecurity: false,
   e2e: {
-    // setupNodeEvents(on, config) {
-    //   // implement node event listeners here
-    // },
+    baseUrl: "http://localhost:5173",
+    setupNodeEvents(on) {
+      on(
+        "file:preprocessor",
+        vitePreprocessor(path.resolve(__dirname, "./vite.config.ts"))
+      );
+    },
     excludeSpecPattern: "**/*.cy-helper.ts",
   },
 });
--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,5 +1,6 @@
 describe("template spec", () => {
   it("passes", () => {
-    cy.visit("https://example.cypress.io");
+    cy.visit("/");
+    cy.findByText("Vite + React").should("exist");
   });
 });
